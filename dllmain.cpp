0de0af1bf9b0fae1ebb4bf5802517cb8f53031d2
Sarbartha Bhowmik <sarbartha31@gmail.com>
2025-04-12 02:34:26 +0530

    Add files via upload

---
+++ dllmain.cpp
@@ -0,0 +1,11 @@
+﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+#include "framework.h"
+#include "module.h"
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    return MBox::OnDllMain(hModule, ul_reason_for_call);
+}
---
+++ framework.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+//从 Windows 头文件中排除极少使用的内容
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <string>
+#include <atomic>
+#include <functional>
+#include <stdexcept>
+#include <array>
+#include <memory>
+#include <mutex>
+
+#include <dwmapi.h>
+#include <CommCtrl.h>
+#include <shlwapi.h>
+#include <Uxtheme.h>
+#pragma comment(lib, "dwmapi.lib")
+#pragma comment(lib, "Comctl32.lib")
+#pragma comment(lib, "Shlwapi.lib")
+#pragma comment(lib, "uxtheme.lib")
